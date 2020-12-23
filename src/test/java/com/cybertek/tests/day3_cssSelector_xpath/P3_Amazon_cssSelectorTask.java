f4553c722a9d4fa74f55aaa06366091a321e45ff
Ibrahim <spyofjava@gmail.com>
2020-12-22 18:51:49 -0800

    dec22

--- src/test/java/com/cybertek/tests/day3_cssSelector_xpath/P3_Amazon_cssSelectorTask.java
+++ src/test/java/com/cybertek/tests/day3_cssSelector_xpath/P3_Amazon_cssSelectorTask.java
@@ -11,6 +11,7 @@ public class P3_Amazon_cssSelectorTask {
     public static void main(String[] args) {
 
         //1. Open Chrome browser
+        //Using our utility to do SETUP + open browser
         WebDriver driver = WebDriverFactory.getDriver("chrome");
 
         //2. Go to https://www.amazon.com
@@ -18,28 +19,30 @@ public class P3_Amazon_cssSelectorTask {
 
         //3. Enter search term (use cssSelector to locate search box)
 
-
         WebElement amazonSearchBar = driver.findElement(By.cssSelector("input[id='twotabsearchtextbox']"));
-       // WebElement amazonSearchBar = driver.findElement(By.cssSelector("input#twotabsearchtextbox"));
-        //tagname[attribute='value']
 
+        //Second syntax for css Selector: tagName#attributeValue
+        //WebElement amazonSearchBar = driver.findElement(By.cssSelector("input#twotabsearchtextbox"));
+        // tagname[attribute='value']
 
-        amazonSearchBar.sendKeys("apple"+ Keys.ENTER);
+        amazonSearchBar.sendKeys("apple" + Keys.ENTER);
 
+        /*
+        How to open search bar in Chrome:
+            MAC: CMD +F
+            WINDOWS: CNTRL + F
+         */
         //4. Verify title contains search term
 
         String expectedInTitle = "apple";
-        String actualInTitle = driver.getTitle();
+        String actualTitle = driver.getTitle();
 
-        if (actualInTitle.contains(expectedInTitle)){
-            System.out.println("Title contains search key. Verification Passed!!");
+        if (actualTitle.contains(expectedInTitle)){
+            System.out.println("Title contains search key. Verification PASSED!");
         }else {
-            System.out.println("Title does NOT contain search key. Verification FAILED!!");
-            System.out.println("actualInTitle = " + actualInTitle);
+            System.out.println("Title does NOT contain search key. Verification FAILED!!!");
+            System.out.println("actualTitle = " + actualTitle);
             System.out.println("expectedInTitle = " + expectedInTitle);
-
-
-            System.out.println();
         }
 
 
