d96f70e4b58156e40383d6bd641176b5b11d05fe
Ibrahim <spyofjava@gmail.com>
2021-01-07 22:55:55 -0800

    Jan_7

--- src/test/java/com/cybertek/tests/day4_findElements_checkboxes/bos.java
+++ src/test/java/com/cybertek/tests/day4_findElements_checkboxes/bos.java
@@ -2,37 +2,44 @@ package com.cybertek.tests.day4_findElements_checkboxes;
 
 import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class bos {
 
 
     public static void main(String[] args) throws InterruptedException {
 
-        WebDriver driver = WebDriverFactory.getDriver("Chrome");
+
+        WebDriver driver=WebDriverFactory.getDriver("chrome");
 
         driver.manage().window().maximize();
-
-        driver.get("https://www.amazon.com");
-        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("apple"+ Keys.ENTER);
-       // WebElement amazonsearchbox=driver.findElement(By.cssSelector("input[id='twotabsearchtextbox']"));
-
-       // amazonsearchbox.sendKeys("apple"+ Keys.ENTER);
+        driver.get("http://practice.cybertekschool.com/forgot_password");
 
 
-        String expectedTitle="apple";
-        String actualtitle=driver.getTitle();
+        WebElement homelink=driver.findElement(By.xpath("//a[.='Home']"));
+        homelink.click();
 
-        if (actualtitle.contains(expectedTitle)){
-            System.out.println("title contains search key. verification PASSED!");
+        Thread.sleep(1000);
+        String actualurl=driver.getCurrentUrl();
+        String expectedUrl="http://practice.cybertekschool.com/";
+
+
+        if (actualurl.equals(expectedUrl)){
+            System.out.println("PASSED");
         }else {
-            System.out.println("title doesn't contains search key. verification FAILED!");
-            System.out.println("Actual title: "+actualtitle);
-            System.out.println("Expected title: "+expectedTitle);
+            System.out.println("FAILED");
+            System.out.println("actual:"+actualurl);
+            System.out.println("expected:"+expectedUrl);
         }
 
-        Thread.sleep(6000);
+
+
+
+
+
+
+               Thread.sleep(2000);
 
 
         driver.close();
