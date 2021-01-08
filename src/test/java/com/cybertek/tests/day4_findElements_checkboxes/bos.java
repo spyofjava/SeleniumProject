7614304d33832fec46febdceaed34d37938262aa
Ibrahim <spyofjava@gmail.com>
2021-01-07 20:57:44 -0800

    Jan_7

--- src/test/java/com/cybertek/tests/day4_findElements_checkboxes/bos.java
+++ src/test/java/com/cybertek/tests/day4_findElements_checkboxes/bos.java
@@ -2,26 +2,25 @@ package com.cybertek.tests.day4_findElements_checkboxes;
 
 import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-
-import java.util.concurrent.TimeUnit;
 
 public class bos {
 
 
     public static void main(String[] args) throws InterruptedException {
 
-        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        WebDriver driver = WebDriverFactory.getDriver("Chrome");
 
         driver.manage().window().maximize();
 
-        driver.get("https://web.whatsapp.com/");
-        Thread.sleep(5000);
-        driver.manage().timeouts().implicitlyWait(18, TimeUnit.SECONDS);
+        driver.get("https://www.amazon.com");
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Apple"+ Keys.ENTER);
 
-        driver.findElement(By.xpath("/html/body/div[1]/div/div/div[3]/div/div[2]/div[1]/div/div/div[13]/div/div/div[2]"));
 
-        Thread.sleep(10000);
+
+
+        Thread.sleep(6000);
 
 
         driver.close();
