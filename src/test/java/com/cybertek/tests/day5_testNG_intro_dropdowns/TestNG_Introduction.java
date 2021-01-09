0845fab0413f2850978583b21f50298602504250
Ibrahim <spyofjava@gmail.com>
2021-01-08 19:32:44 -0800

    Jan_8

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
@@ -1,7 +1,7 @@
 package com.cybertek.tests.day5_testNG_intro_dropdowns;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 public class TestNG_Introduction {
 
@@ -15,6 +15,14 @@ public class TestNG_Introduction {
         System.out.println("--->After method is running...");
     }
 
+
+    /*
+      @BeforeClass will make the method run once before everything else in the class.
+       */
+    @BeforeClass
+    public void setupClass(){
+        System.out.println("------>Before class is running...");
+    }
 
 
 
