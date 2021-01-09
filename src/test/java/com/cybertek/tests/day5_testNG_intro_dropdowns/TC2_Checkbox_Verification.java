20eb3e92703bce19fa1dbae25b64416875e7ca2c
Ibrahim <spyofjava@gmail.com>
2021-01-08 19:32:11 -0800

    Jan_8

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TC2_Checkbox_Verification.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TC2_Checkbox_Verification.java
@@ -9,7 +9,6 @@ import java.util.concurrent.TimeUnit;
 
 public class TC2_Checkbox_Verification {
     public static void main(String[] args) throws InterruptedException {
-
         //1. Open Chrome browser
         WebDriver driver = WebDriverFactory.getDriver("chrome");
 
@@ -34,9 +33,9 @@ public class TC2_Checkbox_Verification {
         //Locating checkbox
         WebElement checkbox1 = driver.findElement(By.xpath("//input[@id='isAgeSelected']"));
 
-        if (!checkbox1.isSelected() && !successMessage.isDisplayed()){
+        if (!checkbox1.isSelected() && !successMessage.isDisplayed()) {
             System.out.println("Checkbox is not selected. Message is not displayed. Verification PASS!");
-        }else{
+        } else {
             System.out.println("FAIL!");
         }
 
@@ -45,11 +44,16 @@ public class TC2_Checkbox_Verification {
 
         checkbox1.click();
 
+        //5. Verify “Success – Check box is checked” message is displayed.
+        if (checkbox1.isSelected() && successMessage.isDisplayed()) {
+            System.out.println("Checkbox is selected. Message is displayed. Verification PASS!");
+        } else {
+            System.out.println("FAIL!");
+        }
+
         Thread.sleep(2000);
 
         driver.close();
-
-
     }
 
 }
---
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
@@ -0,0 +1,16 @@
+package com.cybertek.tests.day5_testNG_intro_dropdowns;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class TestNG_Introduction {
+
+    @BeforeMethod
+    public void setupMethod(){
+        System.out.println("--->Before method is running...");
+    }
+
+
+
+
+}
