efb0ca84829c1d48bd45ebb33162916fc6f99c74
Ibrahim <spyofjava@gmail.com>
2021-01-08 19:32:27 -0800

    Jan_8

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
@@ -1,13 +1,18 @@
 package com.cybertek.tests.day5_testNG_intro_dropdowns;
 
-import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 public class TestNG_Introduction {
 
     @BeforeMethod
     public void setupMethod(){
         System.out.println("--->Before method is running...");
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("--->After method is running...");
     }
 
 
