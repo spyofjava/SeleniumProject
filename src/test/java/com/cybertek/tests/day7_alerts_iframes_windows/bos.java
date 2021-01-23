90dd566f58188e0ed497f1aebd7a4b770482292b
Ibrahim <spyofjava@gmail.com>
2021-01-22 19:27:21 -0800

    Jan_22

--- src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/bos.java
+++ src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/bos.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -42,8 +43,11 @@ public class bos {
 
 
 
+         WebElement resultTest= driver.findElement(By.id("result"));
 
-    }
+
+
+     }
 
 
     @AfterMethod
