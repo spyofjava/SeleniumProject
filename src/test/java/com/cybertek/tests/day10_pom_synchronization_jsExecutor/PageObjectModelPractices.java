0648a62e2d0d3d38c4f0743b0d1d88e0c0e81398
spyofjava <73192225+spyofjava@users.noreply.github.com>
2021-02-08 11:49:43 -0800

    Feb8

--- src/test/java/com/cybertek/tests/day10_pom_synchronization_jsExecutor/PageObjectModelPractices.java
+++ src/test/java/com/cybertek/tests/day10_pom_synchronization_jsExecutor/PageObjectModelPractices.java
@@ -1,4 +1,30 @@
 package com.cybertek.tests.day10_pom_synchronization_jsExecutor;
 
+import com.cybertek.pages.LoginPage;
+import com.cybertek.utilities.Driver;
+import org.testng.annotations.Test;
+
 public class PageObjectModelPractices {
+
+    @Test
+    public void login_to_smartBear(){
+        Driver.getDriver().get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+
+        //When we create object of this class, the object and driver instance will be
+        // already initialized (recognize each other.)
+        LoginPage loginPage = new LoginPage();
+
+        //We are allowed to use object of this class to reach web elements and use selenium methods
+
+        //Sending username using object of LoginPage and web element (that has been located in LoginPage)
+        loginPage.inputUsername.sendKeys("tester");
+
+        loginPage.inputPassword.sendKeys("test");
+
+        loginPage.loginButton.click();
+
+
+
+    }
+
 }
